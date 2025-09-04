0c9aff68972228d7e5231c4398e520ae2b1e2caa
Pasha <39154112+ktkv419@users.noreply.github.com>
2025-09-04 09:33:04 +0000

    wip

--- bookmark/src/App.jsx
+++ bookmark/src/App.jsx
@@ -1,20 +1,61 @@
 import { useState } from 'react'
 import './App.css'
+import { nanoid } from "nanoid"
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  const handleAdd = () => {
-    setCounter((value) => value + 1)
+  const [string, setString] = useState("")
+  const [bookmarks, setBookmarks] = useState([])
+
+  const categories = {
+    movies: "Фильмы",
+    music: "Музыка",
+    site: "Сайты"
   }
-  const handleSub = () => {
-    setCounter((v) => v - 1)
+
+  const [category, setCategory] = useState(Object.entries(categories)[0][0])
+
+
+
+  const handleChange = (e) => {
+    setString(e.target.value)
+  }
+
+  const handleAdd = () => {
+    const bookmark = {
+      name: string,
+      id: nanoid(),
+      date: new Date(),
+      ready: false,
+      category: category
+    }
+    setBookmarks((val) => [bookmark, ...val])
+    console.log(bookmarks);
+  }
+  const handleCatChange = (e) => {
+    setCategory(e.target.value)
   }
   return (
-    <>
-      <button onClick={handleAdd}>Add</button>
-      <h1>{counter}</h1>
-      <button onClick={handleSub}>Subtract</button>
-    </>
+    <div className="bookmarks">
+      <div className="input">
+        <input onChange={handleChange} value={string} type="text" />
+        <select onChange={handleCatChange} name="" id="">
+          {Object.entries(categories).map((el) => (
+            <option value={el[0]}>{el[1]}</option>
+          ))}
+        </select>
+        <button onClick={handleAdd}>Add bookmark</button>
+      </div>
+      <div className="bookmark-list">
+        {bookmarks.map((el) => (
+          <div className="bookmark">
+            <input className="bookmark__ready" type="checkbox" checked={el.ready} />
+            <span className="bookmark__name">{el.name}</span>
+            <span className="bookmark__cat">{categories[el.category]}</span>
+            <span className="bookmark__date">{el.date.toISOString()}</span>
+            </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
---
+++ main.js
@@ -0,0 +1,7 @@
+const categories = {
+    movies: "Фильмы",
+    music: "Музыка",
+    site: "Сайты"
+  }
+
+console.log(Object.entries(categories))
